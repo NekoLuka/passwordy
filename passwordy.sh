92cc7ef22a94e25773615fe63950edfea76a2fed
Luka de Jong <52010222+NekoLuka@users.noreply.github.com>
2023-10-13 21:45:20 +0200

    Added support for $EDITOR

--- passwordy.sh
+++ passwordy.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 umask 077
+set -e
 
 password_file=~/passwords.enc
 temp_file=$(mktemp)
+default_editor=${EDITOR:-nano}
 
 read -sp 'Password: ' password
 
 if [ ! -e $password_file ] || [ ! -s $password_file ]; then
-        nano $temp_file && openssl enc -aes-256-cbc -pbkdf2 -pass pass:$password -in $temp_file -out $password_file && rm $temp_file
+        $default_editor $temp_file && openssl enc -aes-256-cbc -pbkdf2 -pass pass:$password -in $temp_file -out $password_file && rm $temp_file
 else
-        openssl enc -aes-256-cbc -pbkdf2 -pass pass:$password -d -in $password_file -out $temp_file && nano $temp_file && \
+        openssl enc -aes-256-cbc -pbkdf2 -pass pass:$password -d -in $password_file -out $temp_file && $default_editor $temp_file && \
         openssl enc -aes-256-cbc -pbkdf2 -pass pass:$password -in $temp_file -out $password_file && rm $temp_file
 fi
