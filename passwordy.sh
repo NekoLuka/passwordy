bcf5b1f3478f6f8f1e3f226db0f7737c3c029f3e
Luka de Jong <52010222+NekoLuka@users.noreply.github.com>
2023-10-13 13:08:52 +0200

    Initial commit

---
+++ passwordy.sh
@@ -0,0 +1,11 @@
+password_file=~/passwords.enc
+temp_file=$(mktemp)
+
+read -sp 'Password: ' password
+
+if [ ! -e $password_file ] || [ ! -s $password_file ]; then
+        nano $temp_file && openssl enc -aes-256-cbc -pbkdf2 -pass pass:$password -in $temp_file -out $password_file && rm $temp_file
+else
+        openssl enc -aes-256-cbc -pbkdf2 -pass pass:$password -d -in $password_file -out $temp_file && nano $temp_file && \
+        openssl enc -aes-256-cbc -pbkdf2 -pass pass:$password -in $temp_file -out $password_file && rm $temp_file
+fi
